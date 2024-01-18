09f37cb05f1995fd6fd873de14290edef3a14a45
MartiZu <martina.zurli@gmail.com>
2024-01-18 23:23:22 +0000

    installer framer motion

--- my-page/app/layout.tsx
+++ my-page/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import Header from "@/components/header";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,11 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} bg-yellow-50 text-gray-950 relative`}
+        className={`${inter.className} bg-yellow-50 text-gray-950 relative h-[5000px]`}
       >
         <div className="h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] z-10 sm:w-[68.75rem]"></div>
         <div className="h-[31.25rem] w-[50rem] rounded-full blur-[10rem] bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] -z-10 sm:w-[68.75rem] md:left-[33rem] lg:left-[28rem] 2xl:left-[-5rem]"></div>
-
+        <Header />
         {children}
       </body>
     </html>
