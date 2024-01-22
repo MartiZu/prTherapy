dc8a03361b25ff83d7183ea5d3dc366036c28891
MartiZu <martina.zurli@gmail.com>
2024-01-22 18:43:03 +0000

    styling nav bar

--- my-page/components/header.tsx
+++ my-page/components/header.tsx
@@ -1,14 +1,43 @@
+"use client";
 import React from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
+
+const links = [
+  { name: "Home", hash: "#home" },
+  { name: "About", hash: "#about" },
+  { name: "Skills", hash: "#skills" },
+  { name: "Contact", hash: "#contact" },
+];
 
 export default function Header() {
   return (
     <header className="z-[999] relative">
-      <div
-        className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-1/2 rounded-none border
+      <motion.div
+        className="fixed top-0 left-1/2 h-[4.5rem] w-1/2 rounded-none border
        border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] s:w-[36rem] sm:rounded-full"
-      >
-        
-      </div>
+        initial={{ y: -100, x: "-50%", opacity: 0 }}
+        animate={{ y: 0, x: "-50%", opacity: 1 }}
+      ></motion.div>
+      <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7] sm:h-[initial] sm:py-0">
+        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-red-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+          {links.map((link) => (
+            <motion.li
+              key={link.hash}
+              className="h-3/4 flex items-center justify-center"
+              initial={{ y: -100, x: "-50%", opacity: 0 }}
+              animate={{ y: 0, x: "-50%", opacity: 1 }}
+            >
+              <Link
+                href={link.hash}
+                className="flex w-full items-center justify-center p-3 hover:text-gray-300"
+              >
+                {link.name}
+              </Link>
+            </motion.li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 }
